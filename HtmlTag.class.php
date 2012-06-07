89a80bb8cdf83a2c4c827868aaef7f55f53f7a74
Brice LE BOULCH <Airmanbzh@gmail.com>
2012-06-07 18:17:42 +0300

    Update master

--- HtmlTag.class.php
+++ HtmlTag.class.php
@@ -1,24 +1,34 @@
 <?php
+/*
+ * @author brice_leboulch
+ */
 class HtmlTag{
-	
+
 	private static $_instance = null;
-	
+
 	private $_top = null;
 	private $_parent = null;
-	
+
 	private $tag = null;
-	private $attributeList = null;
+	public $attributeList = null;
 	private $classList = null;
-	
+
 	private $content = null;
 	private $text = '';
-	
+
 	private $autoclosed = false;
-	
+
 	private $autocloseTagsList = array(
 		'img','br','hr','input','area','link','meta','param'
 	);
-	
+
+	/**
+	 * 
+	 * Constructor
+	 * @param mixed $tag
+	 * @param HtmlTag $top
+	 * @return htmlTagInstance
+	 */
 	private function __construct($tag, $top = null){
 		$this->tag = $tag;
 		$this->_top =& $top;
@@ -28,12 +38,24 @@ class HtmlTag{
 		$this->text = '';
 		return $this;
 	}
-	
+	/**
+	 * 
+	 * Create a new HtmlTag
+	 * @param string $tag
+	 * @return htmlTagInstance
+	 */
+
 	public static function createElement($tag = ''){
 		self::$_instance = new HtmlTag($tag);
 		return self::$_instance;
 	}
-	
+	/**
+	 * 
+	 * Add element at an existing HtmlTag 
+	 * @param HtmlTag $tag
+	 * @return htmlTagInstance
+	 */
+
 	public function addElement($tag){
 		$htmlTag = null;
 		$this->autoclosed = in_array($this->tag,$this->autocloseTagsList);
@@ -49,23 +71,44 @@ class HtmlTag{
 		$htmlTag->_parent = &$this;
 		return $htmlTag;
 	}
-	
+	/**
+	 * 
+	 * (Re)Define an attribute
+	 * @param string $name
+	 * @param string $value
+	 */
+
 	public function set($name,$value){
 		if(is_null($this->attributeList)) $this->attributeList = array();
 		$this->attributeList[$name] = $value;
 		return $this;
 	}
-	
+	/**
+	 * 
+	 * Shortcut to set('id',$value)
+	 * @param string $value
+	 */
+
 	public function id($value){
 		return $this->set('id',$value);
 	}
-	
+	/**
+	 * 
+	 * Add a class to classList
+	 * @param string $value
+	 */
+
 	public function addClass($value){
 		if(is_null($this->classList))
 			$this->classList = array();
 		$this->classList[] = $value;
 		return $this;
 	}
+	/**
+	 * 
+	 * Remove a class from classList
+	 * @param string $value
+	 */
 
 	public function removeClass($value){
 		if(!is_null($this->classList)){
@@ -73,8 +116,13 @@ class HtmlTag{
 		}
 		return $this;
 	}
-	
-	public function setText($value){
+	/**
+	 * 
+	 * Define text content
+	 * @param string $value
+	 */
+
+	public function text($value){
 		$find = false;
 		foreach($this->content as $k=>$v){
 			if($this->content[$k]->tag == ''){
@@ -85,23 +133,35 @@ class HtmlTag{
 		if(!$find) $this->addElement('')->text = $value;
 		return $this;
 	}
-	
+	/**
+	 * (Deprecated)
+	 * Define text is generated before content 
+	 * @param boolean $bool
+	 */
+
 	public function showTextBeforeContent($bool){
 		$this->textFirst = $bool;
 	}
-	
-	/*
-	*	DOM manipulation
-	*/
-	// Return parent of current object
+	/**
+	 * 
+
+
+
+
+	 * Return parent of current element
+	 */
 	public function getParent(){
 		return $this->_parent;
 	}
-	// Return first child of parent of current object
+	/**
+	 * Return first child of parent of current object
+	 */ 
 	public function getFirst(){
 		return is_null($this->_parent) ? null : $this->_parent->content[0];
 	}
-	// Return last child of parent of current object
+	/**
+	 * Return last child of parent of current object
+	 */
 	public function getPrevious(){
 		$prev = null;
 		$find = false;
@@ -118,7 +178,10 @@ class HtmlTag{
 		}
 		return $prev;
 	}
-	// Return last child of parent of current object
+	/**
+	 * 
+	 * Return last child of parent of current object
+	 */
 	public function getNext(){
 		$next = null;
 		$find = false;
@@ -129,16 +192,22 @@ class HtmlTag{
 					break;
 				}
 				if( $c == $this ) $find = true;
-				
+
 			}
 		}
 		return $next;
 	}
-	// Return last child of parent of current object
+	/**
+	 * 
+	 * Return last child of parent of current object
+	 */
 	public function getLast(){
 		return is_null($this->_parent) ? null : $this->_parent->content[count($this->_parent->content) - 1];
 	}
-	// Delete current child from parent
+	/**
+	 * 
+	 * Delete current child from parent
+	 */
 	public function remove(){
 		$parent = $this->_parent;
 		if(!is_null($parent)){
@@ -147,19 +216,22 @@ class HtmlTag{
 					unset($parent->content[$key]);
 					return $parent;
 				}
-				
+
 			}
 		}
 		return null;
 	}
-	
+
 	/**
-	*	methods of generation
-	**/
+	 * 
+	 * Generation method
+	 */
+
+
 	public function __toString(){
 		return (is_null($this->_top) ? $this->toString() : $this->_top->toString() );
 	}
-	
+
 	public function toString(){
 		$string = '';
 		if(!empty($this->tag)){
@@ -174,12 +246,12 @@ class HtmlTag{
 		}		
 		return $string;
 	}
-	
+
 	private function attributesToString(){
 		$string = '';
 		if(!is_null($this->attributeList)){
 			foreach($this->attributeList as $key => $value){
-				if(!empty($value))
+				if(!is_null($value))
 					$string .= ' ' . $key . '="' . $value . '"';
 			}
 		}
@@ -188,7 +260,7 @@ class HtmlTag{
 		}
 		return $string;
 	}
-	
+
 	private function contentToString(){
 		$string = '';
 		if(!is_null($this->content)){
