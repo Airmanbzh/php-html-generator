7a2fcb3ee3cd6ad0676582e26b9693702d898311
Le Boulch Brice <bleboulch@voyages-sncf.com>
2016-10-28 17:22:05 +0200

    Add test directory

---
+++ tests/MarkupTest.php
@@ -0,0 +1,148 @@
+<?php
+class MarkupTest extends PHPUnit_Framework_TestCase
+{
+//    public function setUp()
+//    {
+//    }
+//
+//    public function tearDown()
+//    {
+//    }
+    public function testCreation()
+    {
+        $this->assertEquals(get_class(HtmlGenerator\Markup::createElement()), "HtmlGenerator\Markup");
+    }
+
+    public function testToString()
+    {
+        $div = HtmlGenerator\Markup::createElement('div');
+        $this->assertEquals($div, '<div></div>');
+    }
+
+    public function testAddElement()
+    {
+        $div = HtmlGenerator\Markup::createElement('div');
+		$div->addElement('p');
+        $this->assertEquals($div, '<div><p></p></div>');
+    }
+
+    public function testAttr()
+    {
+        $div = HtmlGenerator\Markup::createElement('div');
+		$div->attr('id', 'testId');
+        $this->assertEquals($div, '<div id="testId"></div>');
+    }
+
+    public function testSet()
+    {
+        $div = HtmlGenerator\Markup::createElement('div');
+		$div->set('id', 'testId');
+        $this->assertEquals($div, '<div id="testId"></div>');
+    }
+
+    public function testSetWithArray()
+    {
+        $div = HtmlGenerator\Markup::createElement('div');
+		$div->set(['id' => 'testId', 'class' => 'test']);
+        $this->assertEquals($div, '<div id="testId" class="test"></div>');
+    }
+
+    public function testText()
+    {
+        $div = HtmlGenerator\Markup::createElement('div')
+			->addElement('p')
+			->text('text');
+
+        $this->assertEquals($div, '<div><p>text</p></div>');
+    }
+
+    public function testGetParent()
+    {
+        $div = HtmlGenerator\Markup::createElement('div')
+			->addElement('p')
+			->addElement('a')
+			->getParent()
+			->addElement('a');
+
+        $this->assertEquals($div, '<div><p><a></a><a></a></p></div>');
+    }
+
+    public function testGetFirst()
+    {
+        $div = HtmlGenerator\Markup::createElement('div')
+			->addElement('p')
+			->addElement('a')
+			->getParent()
+			->addElement('a')
+			->getFirst()
+			->text('test');
+
+        $this->assertEquals($div, '<div><p><a>test</a><a></a></p></div>');
+    }
+
+    public function testGetLast()
+    {
+        $div = HtmlGenerator\Markup::createElement('div')
+			->addElement('p')
+			->addElement('a')
+			->getParent()
+			->addElement('a')
+			->getParent()
+			->addElement('a')
+			->getFirst()
+			->getLast()
+			->text('test');
+
+        $this->assertEquals($div, '<div><p><a></a><a></a><a>test</a></p></div>');
+    }
+
+    public function testGetPrevious()
+    {
+        $div = HtmlGenerator\Markup::createElement('div')
+			->addElement('p')
+			->addElement('a')
+			->getParent()
+			->addElement('a')
+			->getParent()
+			->addElement('a')
+			->getPrevious()
+			->text('test');
+
+        $this->assertEquals($div, '<div><p><a></a><a>test</a><a></a></p></div>');
+    }
+
+    public function testGetNext()
+    {
+        $div = HtmlGenerator\Markup::createElement('div')
+			->addElement('p')
+			->addElement('a')
+			->getParent()
+			->addElement('a')
+			->getParent()
+			->addElement('a')
+			->getFirst()
+			->getNext()
+			->text('test');
+
+        $this->assertEquals($div, '<div><p><a></a><a>test</a><a></a></p></div>');
+    }
+
+    public function testGetTop()
+    {
+        $div = HtmlGenerator\Markup::createElement('div')
+			->addElement('p')
+			->addElement('a')
+			->getTop()
+			->text('test');
+
+        $this->assertEquals($div, '<div><p><a></a></p>test</div>');
+    }
+
+    public function testRemove()
+    {
+        $div = HtmlGenerator\Markup::createElement('div')
+			->text('test');
+
+        $this->assertEquals(HtmlGenerator\Markup::unXSS($div), '&lt;div&gt;test&lt;/div&gt;');
+    }
+}
