4b22c92335edd0184c2cbf0fb605c0bef07026f1
Le Boulch Brice <bleboulch@voyages-sncf.com>
2016-10-31 11:58:25 +0100

    Add src folder

--- tests/autoloader.php
+++ tests/autoloader.php
@@ -4,10 +4,11 @@ function loader($class)
 	$class = explode('\\', $class);
 	$class = array_pop($class);
 
-
-    $file = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . $class . '.php';
+    $file = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $class . '.php';
+	
     if (file_exists($file)) {
         require $file;
     }
 }
+
 spl_autoload_register('loader');
