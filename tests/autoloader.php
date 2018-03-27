cd86338b89a84edadd7a27882640c7b7cee65e01
Brice Le Boulch <BLeBoulch@voyages-sncf.com>
2018-03-27 16:42:21 +0200

    Correction on php 5.3 unit test classname

--- tests/autoloader.php
+++ tests/autoloader.php
@@ -1,7 +1,13 @@
 <?php
 // backward compatibility
-if (!class_exists('\PHPUnit\Framework\TestCase') && class_exists('\PHPUnit_Framework_TestCase')) {
-    class_alias('PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+if (!class_exists('\PHPUnit\Framework\TestCase')) {
+    if (class_exists('\PHPUnit_Framework_TestCase')) {
+        class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+    }
+
+    if (class_exists('PHPUnit_Framework_TestCase')) {
+        class_alias('PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+    }
 }
 
 function loader($class)
